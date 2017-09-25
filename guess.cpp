76a3710346542c0dc97dc18c87b339f999d06477
rlamug <rlamug@mail.csuchico.edu>
2017-09-25 16:14:57 -0700

    Number Guesser with play again option

--- guess.cpp
+++ guess.cpp
@@ -16,14 +16,24 @@ int main () {
    */
    randomNumber= rand() % 10 + 1;
    int guessedNumber; // this variable will hold the player's guessed number
-   
-   cout << "Guess the computer's number between 1-10" << endl;
-   cin >> guessedNumber; 
-   
-   if( randomNumber == guessedNumber ) {
+   int answer; // this variable will hold the answer to the question if the player want to play again
+
+
+   do {
+   cout << "Guess the computer's number between 1-10." << endl;      
+   cin >> guessedNumber; //Player puts in guessed number
+
+   if ( randomNumber == guessedNumber ) {
       cout << "You guessed correctly! You win!" << endl;
    } else {
       cout << "You were off by " << abs(randomNumber - guessedNumber) << ". Sorry, you lose." << endl;
    }
+   cout << "Would you like to play again? Type in 1 to play again or type in any other number to quit." << endl;
+   cin >> answer;
+
+   } while (answer == 1 );
+   
+   cout << "Thanks for playing. Goodbye." << endl;
+
    return 0;
 }
